0364dff924a53e5cd89e68c6f2deee59e40ed942
Viacheslav Parubtsev <slavaparybcev2003@gmail.com>
2023-03-31 11:02:48 +0300

    feat: section changes

--- src/components/PlusesOfWorking.jsx
+++ src/components/PlusesOfWorking.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../styles/PlusesOfWorking.scss';
 import {PlusesOfWorkingCard} from "./PlusesOfWorkingCard";
+import uiEl from '../assets/pluses_of_working/ui-element.svg';
+import uiBotEl from '../assets/pluses_of_working/ui-bottom-element.svg';
 
 export const PlusesOfWorking = () => {
     const data = [
@@ -13,14 +15,17 @@ export const PlusesOfWorking = () => {
     ]
 
     return (
-        <div className={'PlusesOfWorking container'}>
-            <h2 className={'title'}>Ваши плюсы от работы с нами</h2>
-            <p className={'text'}>Успешно выстроенные бизнес-процессы создают положительную репутацию.
-                <br/>И мы хотим поделиться ею с вами!</p>
-            <div className={'cards'}>
-                {data.map((cardItem, id) => <PlusesOfWorkingCard cardText={cardItem} counter={++id}/>)}
+        <div className={'PlusesOfWorking'}>
+            <img src={uiEl} alt="" className={'ui-element'}/>
+            <div className={'container'}>
+                <h2 className={'title'}>Ваши плюсы от работы с нами</h2>
+                <p className={'text'}>Успешно выстроенные бизнес-процессы создают положительную репутацию.
+                    <br/>И мы хотим поделиться ею с вами!</p>
+                <div className={'cards'}>
+                    {data.map((cardItem, id) => <PlusesOfWorkingCard cardText={cardItem} counter={++id}/>)}
+                </div>
             </div>
-
+            <img src={uiBotEl} alt="" className={'ui-element'}/>
         </div>
     );
 }
