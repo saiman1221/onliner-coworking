19cf4c0175018d521169fe2e8e26f4ca18f83c04
Viacheslav Parubtsev <slavaparybcev2003@gmail.com>
2023-04-03 12:44:34 +0300

    feat: add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import '../styles/Footer.scss';
+import logo from '../assets/footer/logo.svg';
 
 export const Footer = () => {
     return (
         <div className={'Footer'}>
-            <div className={'container'}>
-dad
+            <div className={'content container'}>
+                <div className={'left'}>
+                    <img
+                        src={logo}
+                        alt="Логотип"
+                        className={'logo-image'}
+                    />
+                    <h2 className={'title'}>КОНТАКТЫ</h2>
+                    <p className={'text'}>Лучшее франчайзинговое предложение!</p>
+                </div>
+                <div className={'right'}>
+                    <ul className={'list'}>
+                        <p className={'list-title'}>Телефоны white room:</p>
+                        <li className={'list-item'}><a href="tel:80295408039">8029 540 80 39</a></li>
+                        <li className={'list-item'}><a href="tel:80292695364">8029 269 53 64</a></li>
+                    </ul>
+                    <ul className={'list'}>
+                        <p className={'list-title'}>Находимся:</p>
+                        <li className={'list-item'}>500 м от ст.м.Кунцевщина</li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
